d1d233a113f87e017e638f02b47688299d8b55ed
Aneeba Hassan <aneebahassan123@gmail.com>
2026-06-20 12:54:22 +0500

    feat: initial MERN setup with client and server

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5000/")
+      .then((res) => res.text())
+      .then((data) => setMessage(data));
+  }, []);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Frontend (React)</h1>
+      <h2>{message}</h2>
+    </div>
+  );
+}
+
+export default App;
